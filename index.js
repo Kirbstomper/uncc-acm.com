a67ccfc68f06ce9af191f10989a0852f8dccd9df
kirbstomper <wingedkraby@gmail.com>
2015-10-20 18:08:00 -0400

    Added my name!

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', express.static('./'));
-
+app.get('/chrissmith',function (req, res) {
+  res.end('Heyo from chris!');
+});
 app.get('/nickbreaton', function (req, res) {
   res.end('Hello from Nick!');
 });
